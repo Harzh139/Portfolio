8f0797327aa9017f5c57e9c564f6fd942aa58eda
harsh <sharmaharshu139@gmail.com>
2026-03-01 17:07:04 +0530

    fix: portfolio text contrast

--- components/contact.tsx
+++ components/contact.tsx
@@ -91,7 +91,7 @@ export function Contact() {
             </a>
 
             <a
-              href="https://drive.google.com/file/d/1EH9fK33xhuB7yyttPkB7KDMxwHTcIIqf/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1vlrFHKuvonDu6cXGPzH-9HJid2YKcH1b/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="group flex items-center justify-between p-4 border border-border bg-card/50 hover:bg-background glow-hover transition-all duration-300 relative overflow-hidden"
